667605abcdfc360d31022015a9cafebcdbe45f06
ljubicasamardzic <ljubica.samardzic22@gmail.com>
2024-07-22 19:24:50 +0200

    Remove file

--- src/playground.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Laracasts\Transcriptions\Transcription;
-
-require '../vendor/autoload.php';
-
-$path = __DIR__.'/../tests/stubs/example.vtt';
-
-var_dump(Transcription::load($path)->htmlLines());
